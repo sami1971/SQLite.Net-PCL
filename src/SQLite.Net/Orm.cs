10d04c8504a8c41088dce8ab5f8e1df813304df3
Julius J. Tyner <colonelduddits@yahoo.com>
2013-07-20 03:55:44 +0200

    Add NotNull attribute and support for extended error codes.
    Fixes praeclarum/sqlite-net#187
    
    Merged-by: Øystein Krog <oystein.krog@gmail.com>

--- src/SQLite.Net/Orm.cs
+++ src/SQLite.Net/Orm.cs
@@ -140,5 +140,12 @@ namespace SQLite.Net
             }
             return DefaultMaxStringLength;
         }
+
+        public static bool IsMarkedNotNull(MemberInfo p)
+        {
+            object[] attrs = p.GetCustomAttributes(typeof(NotNullAttribute), true);
+            return attrs.Length > 0;
+        }
+
     }
 }
